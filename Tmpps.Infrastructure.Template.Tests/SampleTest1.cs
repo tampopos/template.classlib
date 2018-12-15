4e10f186c12ada519c07ce68d1ddd604cb6852a0
wakuwaku3 <takushi_119@live.jp>
2018-12-15 16:08:32 +0900

    mstestにした

--- Tmpps.Infrastructure.Template.Tests/SampleTest1.cs
+++ Tmpps.Infrastructure.Template.Tests/SampleTest1.cs
@@ -1,18 +1,21 @@
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tmpps.Infrastructure.Template;
 
 namespace Tests
 {
+    [TestClass]
     public class Tests
     {
-        [TestCaseSource(nameof(GetHelloTestParameters))]
+        [DataTestMethod]
+        [DynamicData(nameof(GetHelloTestParameters), DynamicDataSourceType.Property)]
         public void GetHelloTest(string name, string expected)
         {
             var sample = new Sample();
             var actual = name == null ? sample.GetHello() : sample.GetHello(name);
             Assert.AreEqual(expected, actual);
         }
-        static object[] GetHelloTestParameters = {
+        static object[] GetHelloTestParameters => new []
+        {
             new object[] { null, "Hello world2." },
             new object[] { "tmpps", "Hello tmpps2." },
         };
